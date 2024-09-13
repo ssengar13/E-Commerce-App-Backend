203ecde61d55b723dfbcb5869749017a5a278c6f
Sonal Sengar <sengarsonal13@gmail.com>
2024-09-13 18:40:57 +0530

    added pagination, filtering, sorting, limiting fields API

--- config/refreshToken.js
+++ config/refreshToken.js
@@ -4,4 +4,4 @@ const generateRefreshToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "3d"});
 };
 
-module.exports = { generateRefreshToken }
+module.exports = { generateRefreshToken };
